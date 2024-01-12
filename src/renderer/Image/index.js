e8b653e38f19ae31cac6dd79efd15788b978d17d
or <or@or.local>
2024-01-12 06:54:47 +0200

    add pr

--- src/renderer/Image/index.js
+++ src/renderer/Image/index.js
@@ -1,123 +1,133 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { EditorState } from 'draft-js';
+import { EditorState, SelectionState, Modifier } from 'draft-js';
 import classNames from 'classnames';
 import Option from '../../components/Option';
 import './styles.css';
 
-const getImageComponent = config => class Image extends Component {
-  static propTypes: Object = {
-    block: PropTypes.object,
-    contentState: PropTypes.object,
-  };
+const getImageComponent = (config) =>
+	class Image extends Component {
+		static propTypes: Object = {
+			block: PropTypes.object,
+			contentState: PropTypes.object,
+		};
 
-  state: Object = {
-    hovered: false,
-  };
+		state: Object = {
+			hovered: false,
+		};
 
-  setEntityAlignmentLeft: Function = (): void => {
-    this.setEntityAlignment('left');
-  };
+		setEntityAlignmentLeft: Function = (): void => {
+			this.setEntityAlignment('left');
+		};
 
-  setEntityAlignmentRight: Function = (): void => {
-    this.setEntityAlignment('right');
-  };
+		setEntityAlignmentRight: Function = (): void => {
+			this.setEntityAlignment('right');
+		};
 
-  setEntityAlignmentCenter: Function = (): void => {
-    this.setEntityAlignment('none');
-  };
+		setEntityAlignmentCenter: Function = (): void => {
+			this.setEntityAlignment('none');
+		};
 
-  setEntityAlignment: Function = (alignment): void => {
-    const { block, contentState } = this.props;
-    const entityKey = block.getEntityAt(0);
-    contentState.mergeEntityData(
-      entityKey,
-      { alignment },
-    );
-    config.onChange(EditorState.push(config.getEditorState(), contentState, 'change-block-data'));
-    this.setState({
-      dummy: true,
-    });
-  };
+		setEntityAlignment: Function = (alignment): void => {
+			const { block, contentState } = this.props;
+			const entityKey = block.getEntityAt(0);
+			contentState.mergeEntityData(entityKey, { alignment });
+			config.onChange(EditorState.push(config.getEditorState(), contentState, 'change-block-data'));
+			this.setState({
+				dummy: true,
+			});
+		};
 
-  toggleHovered: Function = (): void => {
-    const hovered = !this.state.hovered;
-    this.setState({
-      hovered,
-    });
-  };
+		removeEntity: Function = (): void => {
+			const { block, contentState } = this.props;
 
-  renderAlignmentOptions(alignment): Object {
-    return (
-      <div
-        className={classNames(
-          'rdw-image-alignment-options-popup',
-          {
-            'rdw-image-alignment-options-popup-right': alignment === 'right',
-          },
-        )}
-      >
-        <Option
-          onClick={this.setEntityAlignmentLeft}
-          className="rdw-image-alignment-option"
-        >
-          L
-        </Option>
-        <Option
-          onClick={this.setEntityAlignmentCenter}
-          className="rdw-image-alignment-option"
-        >
-          C
-        </Option>
-        <Option
-          onClick={this.setEntityAlignmentRight}
-          className="rdw-image-alignment-option"
-        >
-          R
-        </Option>
-      </div>
-    );
-  }
+			const blockKey = block.getKey();
+			const afterKey = contentState.getKeyAfter(blockKey);
+			const targetRange = new SelectionState({
+				anchorKey: blockKey,
+				anchorOffset: 0,
+				focusKey: afterKey,
+				focusOffset: 0,
+			});
+			let newContentState = Modifier.setBlockType(contentState, targetRange, 'unstyled');
 
-  render(): Object {
-    const { block, contentState } = this.props;
-    const { hovered } = this.state;
-    const { isReadOnly, isImageAlignmentEnabled } = config;
-    const entity = contentState.getEntity(block.getEntityAt(0));
-    const { src, alignment, height, width, alt } = entity.getData();
+			newContentState = Modifier.removeRange(newContentState, targetRange, 'backward');
+			config.onChange(EditorState.push(config.getEditorState(), newContentState, 'remove-range'));
+		};
 
-    return (
-      <span
-        onMouseEnter={this.toggleHovered}
-        onMouseLeave={this.toggleHovered}
-        className={classNames(
-          'rdw-image-alignment',
-          {
-            'rdw-image-left': alignment === 'left',
-            'rdw-image-right': alignment === 'right',
-            'rdw-image-center': !alignment || alignment === 'none',
-          },
-        )}
-      >
-        <span className="rdw-image-imagewrapper">
-          <img
-            src={src}
-            alt={alt}
-            style={{
-              height,
-              width,
-            }}
-          />
-          {
-            !isReadOnly() && hovered && isImageAlignmentEnabled() ?
-              this.renderAlignmentOptions(alignment)
-              :
-              undefined
-          }
-        </span>
-      </span>
-    );
-  }
-};
+		toggleHovered: Function = (): void => {
+			const hovered = !this.state.hovered;
+			this.setState({
+				hovered,
+			});
+		};
+
+		renderAlignmentOptions(): Object {
+			return (
+				<React.Fragment>
+					<Option onClick={this.setEntityAlignmentLeft} className='rdw-image-alignment-option'>
+						L
+					</Option>
+					<Option onClick={this.setEntityAlignmentCenter} className='rdw-image-alignment-option'>
+						C
+					</Option>
+					<Option onClick={this.setEntityAlignmentRight} className='rdw-image-alignment-option'>
+						R
+					</Option>
+				</React.Fragment>
+			);
+		}
+
+		renderDeletionOption(): Object {
+			return (
+				<Option onClick={this.removeEntity} className='rdw-image-deletion-option'>
+					X
+				</Option>
+			);
+		}
+
+		render(): Object {
+			const { block, contentState } = this.props;
+			const { hovered } = this.state;
+			const { isReadOnly, isImageAlignmentEnabled, isImageDeletionEnabled } = config;
+			const entity = contentState.getEntity(block.getEntityAt(0));
+			const { src, alignment, height, width, alt } = entity.getData();
+			const isAlignmentEnabled = isImageAlignmentEnabled();
+			const isDeletionEnabled = isImageDeletionEnabled();
+
+			return (
+				<span
+					onMouseEnter={this.toggleHovered}
+					onMouseLeave={this.toggleHovered}
+					className={classNames('rdw-image', {
+						'rdw-image-left': alignment === 'left',
+						'rdw-image-right': alignment === 'right',
+						'rdw-image-center': !alignment || alignment === 'none',
+					})}
+				>
+					<span className='rdw-image-imagewrapper'>
+						<img
+							src={src}
+							alt={alt}
+							style={{
+								height,
+								width,
+							}}
+						/>
+						{!isReadOnly() && hovered && (isAlignmentEnabled || isDeletionEnabled) ? (
+							<div
+								className={classNames('rdw-image-options-popup', {
+									'rdw-image-options-popup-right': alignment === 'right',
+								})}
+							>
+								{isAlignmentEnabled ? this.renderAlignmentOptions() : false}
+								{isDeletionEnabled ? this.renderDeletionOption() : false}
+							</div>
+						) : undefined}
+					</span>
+				</span>
+			);
+		}
+	};
 
 export default getImageComponent;
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,27 +1,37 @@
 import Embedded from './Embedded';
+import Code from './code';
 import getImageComponent from '../renderer/Image';
 
-const getBlockRenderFunc = (config, customBlockRenderer) => (block) => {
-  if (typeof customBlockRenderer === 'function') {
-    const renderedComponent = customBlockRenderer(block, config, config.getEditorState);
-    if (renderedComponent) return renderedComponent;
-  }
-  if (block.getType() === 'atomic') {
-    const contentState = config.getEditorState().getCurrentContent();
-    const entity = contentState.getEntity(block.getEntityAt(0));
-    if (entity && entity.type === 'IMAGE') {
-      return {
-        component: getImageComponent(config),
-        editable: false,
-      };
-    } else if (entity && entity.type === 'EMBEDDED_LINK') {
-      return {
-        component: Embedded,
-        editable: false,
-      };
-    }
-  }
-  return undefined;
+const getBlockRenderFunc = (config, customBlockRenderer, translations) => (block) => {
+	if (typeof customBlockRenderer === 'function') {
+		const renderedComponent = customBlockRenderer(block, config, config.getEditorState);
+		if (renderedComponent) return renderedComponent;
+	}
+	if (block.getType() === 'atomic') {
+		const contentState = config.getEditorState().getCurrentContent();
+		const entity = contentState.getEntity(block.getEntityAt(0));
+		if (entity && entity.type === 'IMAGE') {
+			return {
+				component: getImageComponent(config),
+				editable: false,
+			};
+		} else if (entity && entity.type === 'EMBEDDED_LINK') {
+			return {
+				component: Embedded,
+				editable: false,
+			};
+		}
+		if (entity && (entity.type === 'SCRIPT' || entity.type === 'DIV')) {
+			return {
+				component: Code,
+				editable: false,
+				props: {
+					codeTranslation: translations['components.controls.blocktype.code'],
+				},
+			};
+		}
+	}
+	return undefined;
 };
 
 export default getBlockRenderFunc;
